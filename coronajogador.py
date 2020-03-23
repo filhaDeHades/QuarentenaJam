ed02817ef0740e7e62d29faca946064d9b859d28
Eduardo <egvcmotta@gmail.com>
2020-03-23 17:36:53 -0300

    corona jogador update

--- coronajogador.py
+++ coronajogador.py
@@ -4,6 +4,7 @@ gravit = 2
 class Coronajogador:
     x = 0
     y = 0
+    vivo = True
     anim = [0,0]
     def __init__(self):
         pyxel.load("assets/corona.pyxres")
@@ -11,10 +12,13 @@ class Coronajogador:
         pyxel.blt(self.x, self.y, 0, self.anim[0], self.anim[1], 15, 15, 0)
     def update(self):
         if pyxel.btn(pyxel.KEY_SPACE):
-            self.y = self.y -16 
-            max.y = 80
-
+            self.y = max(self.y -16 , 0)
         else:
-            self.y = self.y +gravit
-            min.y = 0
-
+            self.y = min(self.y +gravit, 80)
+        
+        if self.y >= 80:
+            self.estado(False)
+        else:
+            self.estado(True)
+    def estado(self,jorge): 
+        self.vivo = jorge
