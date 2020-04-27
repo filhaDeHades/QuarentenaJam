b0df783131d5a8aca5ad19e9d2bf7ecf51c58fed
Amanda <abmelo2010@gmail.com>
2020-04-27 18:59:13 -0300

    obstaculo att

--- jogo.py
+++ jogo.py
@@ -4,6 +4,8 @@ import obst as obs
 import fundo
 from random import randint
 posY = [0, 16, 32, 48, 64] #Posições fixas do Y
+dist = [60, 50, 40, 35, 30, 25, 20, 15] #Distâncias entre os obstáculos
+dif = len(dist) #Dificuldade relacionada a distância
 
 class Jogo:
     player = cj.Coronajogador(0, 0)
@@ -23,6 +25,8 @@ class Jogo:
         self.height = h
         self.status = "JOGANDO"
         self.pontos = 0
+        self.dif = 0            #Dificuldade relacionada a distância
+        self.av = False         #Avançar o nível de dificuldade
         for i in self.objetos:
             i.setX(180)
         a = fundo.Fundo(0,0,180,80,0)
@@ -32,25 +36,33 @@ class Jogo:
         pyxel.mouse(False)
         self.mouse()
         self.contframes += 1
+        if (self.av & (self.dif<dif)):
+            self.av = False
+            self.dif += 1
+            #print("Avançou para {}".format(str(self.dif))
+
     # ----------------------------- SETANDO OBSTÁCULOS --------------------------------
-        if self.contframes == 60:
+        if self.contframes >= dist[self.dif]:
             self.contframes = 0
-            alt = randint(0, 4)
             obj = obs.Obstaculos(self.status, posY[randint(0, 4)], randint(0,7))
             obj.setX(180)
             self.objetos.append(obj)
+            #print("Tenho {} objetos".format(len(self.objetos)))
+
     # -------------------------- CONFIGURAÇÃO DO JOGADOR -------------------------------
         self.player.update()
         #if self.player.update() == 1:
             #self.animplayer = 1
+
     # ------------------------ CONFIGURAÇÃO DOS OBSTÁCULOS -----------------------------
-        for i in self.objetos:
-            i.update()
+        for o in self.objetos:
+            o.update()
         for i in range(len(self.objetos)):
             if self.objetos[i].obsX < 4:
-                self.pontos += 5
+                self.pontos += 5 + self.dif   # Pontuação fixa + nível de dificuldade
                 del(self.objetos[i])
                 break
+
     # ------------------------ CONFIGURAÇÃO DE COLISÕES --------------------------------
         for i in self.objetos:
             if self.player.x > i.obsX and self.player.x <= i.obsX+16:
@@ -63,7 +75,6 @@ class Jogo:
                 for j in range(len(self.objetos)):
                         self.objetos[j].setState("JOGANDO")
 
-
     # -------------------------- CONFIGURAÇÃO DO FUNDO ---------------------------------
         for i in range(len(self.fundos)):
             if self.fundos[i].x + self.fundos[i].w == 180:
@@ -73,6 +84,9 @@ class Jogo:
                 else:
                     a = fundo.Fundo(181,0,180,80,0)
                     self.fundos.append(a)
+                    # Quando volta pra China
+                    if (self.dif < dif): # Avançar nível de dificuldade
+                        self.av = True
             if self.fundos[i].x + self.fundos[i].w <= 0:
                 del(self.fundos[i])
                 break
@@ -108,3 +122,5 @@ class Jogo:
         self.contframes = 0
         self.animplayer = 0
         self.count = 0
+        self.dif = 0
+        self.av = False
